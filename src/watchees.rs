8c77fd4e0011bad1c6b5429893153d326525393a
misonomi <misonomi0612@gmail.com>
2019-03-12 16:13:00 +0900

    implemented a method
    watchees::interpret_line()

--- src/watchees.rs
+++ src/watchees.rs
@@ -1,13 +1,18 @@
 use std::fs::File;
 use std::io::{ BufReader, BufRead, Error };
 use std::sync::Mutex;
+use std::time::Instant;
 use lazy_static::lazy_static;
 
 use serenity::model::{ id::UserId, gateway::Game };
 
+mod stat;
+
 pub struct Watchee {
     id: UserId,
-    game: Option<Game>
+    stat: stat::BondType,
+    game: Option<Game>,
+    last_update: Instant
 }
 
 lazy_static! {
@@ -16,20 +21,38 @@ lazy_static! {
         
         let watchee_reader = BufReader::new(File::open("watchees.dat").expect("no file: 'watchees.dat'"));
         for raw_watchee in watchee_reader.lines() {
-            watchlist.push(Watchee{ id: interpret_line(raw_watchee), game: None });
+            if let Some(watchee) = interpret_line(&raw_watchee) {
+                watchlist.push(watchee);
+            }
         }
         Mutex::new(watchlist)
     };
 }
 
-fn interpret_line(_lineresult: Result<String, Error>) -> UserId {
-    UserId::from(1)
+fn interpret_line(line: &Result<String, Error>) -> Option<Watchee> {
+    // TODO lines().expect() and not match?
+    // do when I wont need cause anymore
+    match line {
+        Ok(line) =>{
+            let raw_props: Vec<&str> = line.split(":").collect();
+            if raw_props.len() != 2 {
+                return None;
+            }
+            let u64id: u64 = raw_props[0].parse().expect("failed to parse userid");
+            let u8stat: u8 = raw_props[1].parse().expect("failed to parse stat");
+            Some(Watchee{ id: UserId::from(u64id), stat: stat::bond_from(u8stat), game: None, last_update: Instant::now() })
+        },
+        Err(cause) => {
+            println!("Error when reading watchees: {:?}", cause);
+            None
+        }
+    }
 }
 
 pub fn add_watchee(id: &UserId) -> Result<usize, usize> {
     if has_watchee(id).is_none() {
         let mut watchees_guarded = WATCHEES.lock().unwrap();
-        watchees_guarded.push(Watchee{ id: *id, game: None });
+        watchees_guarded.push(Watchee{ id: *id, stat: stat::BondType::normal, game: None, last_update: Instant::now() });
         Ok(watchees_guarded.capacity())
     } else {
         Err(0)
---
+++ src/watchees/stat.rs
@@ -0,0 +1,15 @@
+pub enum BondType {
+    normal,
+    intimate,
+    admin,
+    unknown
+}
+
+pub fn bond_from(id: u8) -> BondType {
+    match id {
+        1 => BondType::normal,
+        2 => BondType::intimate,
+        0 => BondType::admin,
+        _ => BondType::unknown
+    }
+}
